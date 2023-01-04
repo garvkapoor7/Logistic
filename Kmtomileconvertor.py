67bdd4f7afc3a0077844aee5a1d68022d2e83811
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-01-04 18:52:07 +0530

    km to miles

---
+++ Kmtomileconvertor.py
@@ -0,0 +1,6 @@
+#take user input for km
+#and convert it into miles and vise versa
+
+distanceTravelled = float(input("No. of distance'km':"))
+conversionIntoMiles = float(distanceTravelled*0.621)
+print("Total distance travelled in miles =",  conversionIntoMiles)
